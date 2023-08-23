eb2b08c1747ace7d1c6b55a3a916a20fbae2f1b8
FanOne <294350394@qq.com>
2023-08-23 23:20:07 +0800

    feat:middleware jwt expired time

--- app/gateway/middleware/jwt.go
+++ app/gateway/middleware/jwt.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/CocaineCong/micro-todoList/pkg/ctl"
@@ -12,24 +15,37 @@ func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var code uint32
 
-		code = 200
+		code = http.StatusOK
 		token := c.GetHeader("Authorization")
 		if token == "" {
-			code = 404
-			c.JSON(500, gin.H{
+			code = http.StatusNotFound
+			c.JSON(http.StatusOK, gin.H{
 				"code": code,
 				"msg":  "鉴权失败",
 			})
+			return
 		}
 		claims, err := utils.ParseToken(token)
 		if err != nil {
-			code = 401
-			c.JSON(500, gin.H{
+			code = http.StatusUnauthorized
+			c.JSON(http.StatusOK, gin.H{
 				"code": code,
 				"msg":  "鉴权失败",
 			})
 			c.Abort()
+			return
 		}
+
+		if time.Now().Unix() > claims.ExpiresAt {
+			code = http.StatusUnauthorized
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  "权限过期，请重新登陆",
+			})
+			c.Abort()
+			return
+		}
+
 		c.Request = c.Request.WithContext(ctl.NewContext(c.Request.Context(), &ctl.UserInfo{Id: claims.Id}))
 		ctl.InitUserInfo(c.Request.Context())
 		c.Next()
